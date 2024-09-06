e80b3a661037bed4f03123e365f70fad806ae96f
vincent anjiri <vincentanjiri12@gmail.com>
2024-09-06 23:58:25 +0300

    "Added import statements and a new method getSummaryData to ResultsScreen, and replaced a Text widget with a QuestionSummary widget in the build method."

---
+++ lib/question_summary.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class QuestionSummary extends StatelessWidget {
+  const QuestionSummary(this.summaryData, {super.key});
+
+  final List<Map<String, Object>> summaryData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: summaryData.map(
+        (data) {
+          return Row(
+            children: [
+              Text(((data['question_index'] as int) + 1).toString()),
+              Column(
+                children: [
+                  Text(data['question'] as String),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(data['user_answer'] as String),
+                  Text(data['correct_answer'] as String),
+                ],
+              ),
+            ],
+          );
+        },
+      ).toList(),
+    );
+  }
+}
--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app_flutter/data/questions.dart';
+import 'package:quiz_app_flutter/question_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
 
   final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(context) {
@@ -18,7 +34,7 @@ class ResultsScreen extends StatelessWidget {
             const SizedBox(
               height: 30,
             ),
-            const Text('List of answers and questions'),
+            QuestionSummary(getSummaryData()),
             const SizedBox(
               height: 30,
             ),
