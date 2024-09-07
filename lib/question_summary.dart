27720f2805d4a2405254c58ed6157faf45006f1a
vincent anjiri <vincentanjiri12@gmail.com>
2024-09-07 23:22:19 +0300

    "Refactor QuestionSummary widget to use Expanded widget and remove selectedAnswers reset in Quiz"

--- lib/question_summary.dart
+++ lib/question_summary.dart
@@ -13,15 +13,17 @@ class QuestionSummary extends StatelessWidget {
           return Row(
             children: [
               Text(((data['question_index'] as int) + 1).toString()),
-              Column(
-                children: [
-                  Text(data['question'] as String),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Text(data['user_answer'] as String),
-                  Text(data['correct_answer'] as String),
-                ],
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(data['question'] as String),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(data['user_answer'] as String),
+                    Text(data['correct_answer'] as String),
+                  ],
+                ),
               ),
             ],
           );
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -31,7 +31,6 @@ class _QuizState extends State<Quiz> {
     if (selectedAnswers.length == questions.length) {
       setState(() {
         activeScreen = 'results-screen';
-        selectedAnswers = [];
       });
     }
   }
